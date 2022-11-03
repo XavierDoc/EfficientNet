a0ec61b16908bd3a89d522955ac279e34790105d
Xavier <sci3@Xavier.local>
2022-11-03 22:17:59 +0800

    please

---
+++ layers.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+from func import Swish
+
+
+
+class MBConv(nn.Module):
+    expand = 6
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, se_scale=4, p=0.5):
+        super().__init__()
+        # first MBConv is not using stochastic depth
+        self.p = torch.tensor(p).float() if (in_channels == out_channels) else torch.tensor(1).float()
+
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels * MBConv.expand, 1, stride=stride, padding=0, bias=False),
+            nn.BatchNorm2d(in_channels * MBConv.expand, momentum=0.99, eps=1e-3),
+            Swish(),
+            nn.Conv2d(in_channels * MBConv.expand, in_channels * MBConv.expand, kernel_size=kernel_size,
+                      stride=1, padding=kernel_size//2, bias=False, groups=in_channels*MBConv.expand),
+            nn.BatchNorm2d(in_channels * MBConv.expand, momentum=0.99, eps=1e-3),
+            Swish()
+        )
+
+        self.se = SEBlock(in_channels * MBConv.expand, se_scale)
+
+        self.project = nn.Sequential(
+            nn.Conv2d(in_channels*MBConv.expand, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(out_channels, momentum=0.99, eps=1e-3)
+        )
+
+        self.shortcut = (stride == 1) and (in_channels == out_channels)
+
+    def forward(self, x):
+        # stochastic depth
+        if self.training:
+            if not torch.bernoulli(self.p):
+                return x
+
+        x_shortcut = x
+        x_residual = self.residual(x)
+        x_se = self.se(x_residual)
+
+        x = x_se * x_residual
+        x = self.project(x)
+
+        if self.shortcut:
+            x= x_shortcut + x
+
+        return x
+
+# SE Block
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, r=4):
+        super().__init__()
+
+        self.squeeze = nn.AdaptiveAvgPool2d((1,1))
+        self.excitation = nn.Sequential(
+            nn.Linear(in_channels, in_channels * r),
+            Swish(),
+            nn.Linear(in_channels * r, in_channels),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.squeeze(x)
+        x = x.view(x.size(0), -1)
+        x = self.excitation(x)
+        x = x.view(x.size(0), x.size(1), 1, 1)
+        return x
+
+
+class SepConv(nn.Module):
+    expand = 1
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, se_scale=4, p=0.5):
+        super().__init__()
+        # first SepConv is not using stochastic depth
+        self.p = torch.tensor(p).float() if (in_channels == out_channels) else torch.tensor(1).float()
+
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels * SepConv.expand, in_channels * SepConv.expand, kernel_size=kernel_size,
+                      stride=1, padding=kernel_size//2, bias=False, groups=in_channels*SepConv.expand),
+            nn.BatchNorm2d(in_channels * SepConv.expand, momentum=0.99, eps=1e-3),
+            Swish()
+        )
+
+        self.se = SEBlock(in_channels * SepConv.expand, se_scale)
+
+        self.project = nn.Sequential(
+            nn.Conv2d(in_channels*SepConv.expand, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(out_channels, momentum=0.99, eps=1e-3)
+        )
+
+        self.shortcut = (stride == 1) and (in_channels == out_channels)
+
+    def forward(self, x):
+        # stochastic depth
+        if self.training:
+            if not torch.bernoulli(self.p):
+                return x
+
+        x_shortcut = x
+        x_residual = self.residual(x)
+        x_se = self.se(x_residual)
+
+        x = x_se * x_residual
+        x = self.project(x)
+
+        if self.shortcut:
+            x= x_shortcut + x
+
+        return x
+
